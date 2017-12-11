995312f650db84b932357df607aadfb9f9d1c737
Shawn Taylor <shawntaylor@weecology.org>
2017-12-11 18:10:22 -0500

    initial

---
+++ pySimpleMPI/examples.py
@@ -0,0 +1,37 @@
+import time
+from pySimpleMPI.framework import run_MPI
+
+class ExampleWorker:
+    def __init__(self):
+        pass
+
+    def setup(self):
+        print('seting things up')
+    
+    def run_job(self, job_details):
+        time.sleep(2)
+        print('running job')
+        return job_details
+        
+class ExampleBoss:
+    def __init__(self):
+        pass
+    
+    def setup(self):
+        self.job_list=list(range(10))
+        self.total_jobs = len(self.job_list)
+    
+    def jobs_available(self):
+        return len(self.job_list)>0
+        
+    def get_next_job(self):
+        return self.job_list.pop()
+    
+    def process_job_result(self, result):
+        pass
+
+    def process_all_results(self, all_results):
+        pass
+        
+if __name__ == "__main__":
+    run_MPI(ExampleBoss(), ExampleWorker())
---
+++ pySimpleMPI/framework.py
@@ -0,0 +1,71 @@
+from mpi4py import MPI
+
+work_tag=0
+stop_tag=1
+
+def worker(worker_class):
+    comm = MPI.COMM_WORLD
+    status = MPI.Status()
+        
+    worker_class.setup()
+    
+    while True:
+        job_details = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+        if status.Get_tag() == stop_tag: break
+
+        job_results = worker_class.run_job(job_details)
+        
+        comm.send(obj=job_results, dest=0)
+
+def boss(boss_class):
+    comm = MPI.COMM_WORLD
+    status = MPI.Status()
+    num_workers = MPI.COMM_WORLD.Get_size()
+
+    boss_class.setup()
+    
+    #Dole out the first round of jobs to all workers
+    for i in range(1, num_workers):
+        if boss_class.jobs_available():
+            next_job = boss_class.get_next_job()
+        else:
+            break
+        comm.send(obj=next_job, dest=i, tag=work_tag)
+    
+    results=[]
+    total_jobs = boss_class.total_jobs
+    jobs_completed = 0
+    #While there are new jobs to assign.
+    #Collect results and assign new jobs as others are finished.
+    while boss_class.jobs_available():
+        job_result = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+        results.append(job_result)
+        next_job = boss_class.get_next_job()
+        comm.send(obj=next_job, dest=status.Get_source(), tag=work_tag)
+    
+        jobs_completed+=1
+        print('Completed job '+str(jobs_completed)+' of '+str(total_jobs))
+        
+    #Collect last jobs
+    for i in range(1, num_workers):
+        job_result = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+        results.append(job_result)
+        
+    #Shut down all workers
+    for i in range(1, num_workers):
+        comm.send(obj=None, dest=i, tag=stop_tag)
+        
+    boss_class.process_all_results(results)
+
+def run_MPI(boss_class, worker_class):
+    rank = MPI.COMM_WORLD.Get_rank()
+    name = MPI.Get_processor_name()
+
+    if rank == 0:
+        print('boss '+str(rank)+' on '+str(name))
+        boss(boss_class)
+    else:
+        print('worker '+str(rank)+' on '+str(name))
+        worker(worker_class)
+
+
