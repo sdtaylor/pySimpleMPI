de1ff9ef03f30020b84550c86b5d6755e217b11c
Shawn Taylor <shawntaylor@weecology.org>
2017-12-11 18:22:57 -0500

    simple python setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pySimpleMPI',
+      version='0.0.1',
+      description='embarrassingly simple parallel tasks with MPI in python',
+      url='https://github.com/sdtaylor/pySimpleMPI',
+      author='Shawn Taylor',
+      author_email='shawntaylor@weecology.org',
+      license='MIT',
+      zip_safe=False)
